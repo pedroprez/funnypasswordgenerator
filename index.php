b5621c557d812fdfad42c07043e6fa12d95b4900
Pedro Prez <pedroprez@keetup.com>
2014-04-12 22:22:26 -0300

    -Loading y algunos fix sueltos

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ ini_set("display_errors", "1");
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap 101 Template</title>
+    <title>Funny Password Generator!</title>
 
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
@@ -43,7 +43,7 @@ ini_set("display_errors", "1");
           <span class="input-group-btn">
             <button class="btn btn-primary" type="button">Generate!</button>
           </span>
-          <input type="text" class="form-control" id="generate-input">
+          <input type="text" class="form-control" id="generate-input" readonly="readonly">
         </div><!-- /input-group -->
       </div>
     </div>
@@ -60,6 +60,7 @@ ini_set("display_errors", "1");
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
+
     <script src="js/bootstrap.min.js"></script>
     <script src="js/main.js"></script>
   </body>
--- js/main.js
+++ js/main.js
@@ -1,8 +1,58 @@
 $(document).ready(function(){
 	$("#generate-form .btn").click(function(){
-		$text = $("#generate-form input[type='text']");
+		var $btn = $(this);
+    	//$btn.button('loading');
+		var $text = $("#generate-form input[type='text']");
+		//$text.val("...");
+		//$text.attr("disable",'disable');
+
+		//document.body.appendChild(circle.canvas);
+		var loading = new Loading($text);
+		loading.play();
+		
 		$.get( "ajax/generate.php?" + Math.random() * 10, function( data ) {
 		  $text.val( data );
+		  loading.stop();
 		});
 	})
 });
+
+loadingElement = null;
+loadingWaitingPoints = 0;
+loadingCurrentTimer = null;
+//loadingWaitingPoints = 3;
+function Loading(element) { 
+	loadingElement = $(element);
+	this.initializate = function () {
+		loadingElement.val("Loading");
+		loadingWaitingPoints = 0;
+	}
+	this.play = function(){
+			loadingElement.attr("disabled",'disabled');
+			this.initializate();
+			
+			loadingCurrentTimer = setInterval(function(){
+				loadingElement.val();
+				loadingElement.val(loadingElement.val() + ".");
+				loadingWaitingPoints++;
+				if (loadingWaitingPoints > 3) {
+					var loadToReset = Loading(loadingElement);
+					this.reset();
+				}
+			}, 500);
+	},
+	this.stop = function(){
+		window.clearInterval(loadingCurrentTimer);
+		loadingElement.removeAttr("disabled");
+	},
+	this.reset = function() {
+		this.stop();
+		this.play();
+	}
+
+}
+
+
+
+
+
